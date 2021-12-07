461e439bf524770269dba8c06fcb62e5a9363959
salehfaraj08 <89396137+salehfaraj08@users.noreply.github.com>
2021-12-07 20:09:59 +0200

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/api/workers', require('./routes/user.route'));
 app.use('/api/shifts', require('./routes/shift.route'));
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.whijz.mongodb.net/workAppDb?retryWrites=true&w=majority`, { useNewUrlParser: true });
-
+console.log(process.env.PORT);
 app.listen(process.env.PORT || port, () => {
     console.log(`Server started on ${process.env.PORT || port}`);
 });
