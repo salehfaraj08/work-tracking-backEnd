bb2917cfff0a015046b7c591dec22a645eb48928
salehfaraj08 <89396137+salehfaraj08@users.noreply.github.com>
2021-12-07 20:18:34 +0200

    update

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 const mongoose = require('mongoose');
 const port = 5001;
-
-bodyParser.urlencoded({ extended: false });
-app.use(bodyParser.json());
 
 /***********************ROUTERS***************************/                        
 app.use('/api/workers', require('./routes/user.route'));
 app.use('/api/shifts', require('./routes/shift.route'));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.whijz.mongodb.net/workAppDb?retryWrites=true&w=majority`, { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://saleh:saleh0811@cluster0.whijz.mongodb.net/workAppDb?retryWrites=true&w=majority`, { useNewUrlParser: true });
 console.log(process.env.PORT);
 app.listen(process.env.PORT || port, () => {
     console.log(`Server started on ${process.env.PORT || port}`);
